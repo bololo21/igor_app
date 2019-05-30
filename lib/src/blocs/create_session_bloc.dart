cec3d4faba95139fd48ac9e6c213388fd6b06b9e
Patrick Beal <patrickvrb@gmail.com>
2019-05-30 16:01:08 -0300

    Datas de criar sessão funcionando e salvando no bd

--- lib/src/blocs/create_session_bloc.dart
+++ lib/src/blocs/create_session_bloc.dart
@@ -6,27 +6,27 @@ import 'package:rxdart/rxdart.dart';
 class CreateSessionBloc extends Bloc {
   final _repository = Repository();
   final _name = BehaviorSubject<String>();
-  final _description = BehaviorSubject<String>();
+  final _date = BehaviorSubject<String>();
 
   Observable<String> get name => _name.stream;
 
-  Observable<String> get description => _description.stream;
+  Observable<String> get date => _date.stream;
 
   // Change data
   Function(String) get changeName => _name.sink.add;
 
-  Function(String) get changeDescription => _description.sink.add;
+  Function(String) get changeDate => _date.sink.add;
 
 
   Future<void> createSession() async {
-    return _repository.createSession(_name.value, DateTime.now().toString());
+    return _repository.createSession(_name.value, _date.value);
   }
 
   void dispose() async {
     await _name.drain();
     _name.close();
-    await _description.drain();
-    _description.close();
+    await _date.drain();
+    _date.close();
   }
 
   static Bloc instance() => CreateSessionBloc();
--- lib/src/screens/create_session.dart
+++ lib/src/screens/create_session.dart
@@ -5,7 +5,6 @@ import 'package:igor_app/src/blocs/bloc_provider.dart';
 import 'package:igor_app/src/blocs/create_session_bloc.dart';
 import 'package:intl/intl.dart';
 
-
 class RegisterSessionScreen extends StatefulWidget {
   @override
   _RegisterSessionScreenState createState() => _RegisterSessionScreenState();
@@ -14,127 +13,145 @@ class RegisterSessionScreen extends StatefulWidget {
 class _RegisterSessionScreenState extends State<RegisterSessionScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final _bloc = $Provider.of<CreateSessionBloc>();
-
-
+  
   @override
   Widget build(BuildContext context) {
     appConfig.setConfig(context);
-      DateTime _date = new DateTime.now();
-      String _dateFormated = DateFormat('dd/MM').format(_date);
-        return Scaffold(
-            body: Stack(
-              children: <Widget>[
-                new Container(
-                  decoration: new BoxDecoration(
-                    image: new DecorationImage(
-                      image: new AssetImage("assets/log_in/Backgrownd.png"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                SingleChildScrollView(
-                  child: Center(
-                      child: Column(
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        new Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage("assets/log_in/Backgrownd.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SingleChildScrollView(
+          child: Center(
+              child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 10 * appConfig.blockSizeVertical,
+              ),
+              Container(
+                decoration: new BoxDecoration(
+                    color: const Color(0xffe2e2e1),
+                    borderRadius: new BorderRadius.only(
+                        topLeft: const Radius.circular(5.0),
+                        topRight: const Radius.circular(5.0))),
+                padding: EdgeInsets.fromLTRB(
+                    2 * appConfig.blockSize,
+                    1 * appConfig.blockSizeVertical,
+                    2 * appConfig.blockSize,
+                    2 * appConfig.blockSizeVertical),
+                margin: EdgeInsets.fromLTRB(
+                    2 * appConfig.blockSize, 0, 2 * appConfig.blockSize, 0),
+                //color: const Color(0xffe2e2e1),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
                         children: <Widget>[
-                          SizedBox(
-                            height: 10 * appConfig.blockSizeVertical,
+                          GestureDetector(
+                            child: Icon(
+                              Icons.close,
+                              color: Colors.black12,
+                            ),
+                            onTap: () => Navigator.pushNamed(
+                                context, '/index_adventure'),
                           ),
-                          Container(
-                            decoration: new BoxDecoration(
-                              color: const Color(0xffe2e2e1),
-                              borderRadius: new BorderRadius.only(
-                              topLeft: const Radius.circular(5.0),
-                              topRight: const Radius.circular(5.0))),
-                            padding: EdgeInsets.fromLTRB(2 * appConfig.blockSize, 1 * appConfig.blockSizeVertical,
-                                2 * appConfig.blockSize, 2 * appConfig.blockSizeVertical),
-                            margin: EdgeInsets.fromLTRB(
-                                2 * appConfig.blockSize, 0, 2 * appConfig.blockSize, 0),
-                            //color: const Color(0xffe2e2e1),
-                            child: Form(
-                              key: _formKey,
-                              child: Column(
-                                children: <Widget>[
-    
-                                  Row(children: <Widget>[
-                                    GestureDetector(
-                                      child: Icon(Icons.close, color: Colors.black12,),
-                                      onTap: () => Navigator.pushNamed(context, '/index_adventure'),
-                                    ),
-                                    SizedBox(width: 2 * appConfig.blockSize),
-                                    Text("Criar Sessão",
-                                      style: TextStyle(color: Colors.teal),),
-                                  ],),
-    
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      left: 20.0,
-                                      right: 20.0,
-                                    ),
-    
-                                    child: Column(
-                                      children: <Widget>[
-                                        StreamBuilder(
-                                            stream: _bloc.name,
-                                            builder:
-                                                (context, AsyncSnapshot<String> snapshot) {
-                                              return TextField(
-                                                onChanged: _bloc.changeName,
-                                                decoration: InputDecoration(
-                                                    labelText: 'De um nome à próxima sessão',
-                                                    labelStyle: TextStyle(
-                                                        color: Colors.black,
-                                                        fontSize: 22,
-                                                        fontFamily: 'Fira-sans'),
-                                                    errorText: snapshot.error),
-                                              );
-                                            }),
-                                      ],
-                                    ),
-                                  ),
-                                  SizedBox(height: 3 * appConfig.blockSizeVertical),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      left: 20.0,
-                                      right: 20.0,
-                                    ),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        MaterialButton(
-                                        child: Text(_dateFormated),
-                                        textColor: const Color(0xffe2e2e1),
-                                        color: Colors.teal,
-                                        onPressed: () {
-                                          _selectDate(context);
-                                        }),
-                                     MaterialButton(
-                                        child: Text("CRIAR"),
-                                        textColor: const Color(0xffe2e2e1),
-                                        color: Colors.teal,
-                                        onPressed: () {
-                                          _bloc.createSession();
-                                          Navigator.pushNamed(context, '/index_adventure');
-                                        }),
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 20.0,
-                                  right: 20.0,
-                                ),
-                              ),
-                            ],
+                          SizedBox(width: 2 * appConfig.blockSize),
+                          Text(
+                            "Criar Sessão",
+                            style: TextStyle(color: Colors.teal),
                           ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20.0,
+                          right: 20.0,
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            StreamBuilder(
+                                stream: _bloc.name,
+                                builder:
+                                    (context, AsyncSnapshot<String> snapshot) {
+                                  return TextField(
+                                    onChanged: _bloc.changeName,
+                                    decoration: InputDecoration(
+                                        labelText:
+                                            'De um nome à próxima sessão',
+                                        labelStyle: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 22,
+                                            fontFamily: 'Fira-sans'),
+                                        errorText: snapshot.error),
+                                  );
+                                }),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 3 * appConfig.blockSizeVertical),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20.0,
+                          right: 20.0,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            MaterialButton(
+                                child: Text("DATA"),
+                                textColor: const Color(0xffe2e2e1),
+                                color: Colors.teal,
+                                onPressed: () {
+                                  _selectDate(context);
+                                }),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20.0,
+                          right: 20.0,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            MaterialButton(
+                                child: Text("PRONTO"),
+                                textColor: const Color(0xffe2e2e1),
+                                color: Colors.teal,
+                                onPressed: () {
+                                  _bloc.createSession();
+                                  Navigator.pushNamed(
+                                      context, '/index_adventure');
+                                })
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20.0,
+                          right: 20.0,
                         ),
                       ),
                     ],
-                  )),
-
-            )
-          ],
-        ));
+                  ),
+                ),
+              ),
+            ],
+          )),
+        )
+      ],
+    ));
   }
+
   @override
   void dispose() {
     $Provider.dispose<CreateSessionBloc>();
@@ -148,10 +165,11 @@ class _RegisterSessionScreenState extends State<RegisterSessionScreen> {
       firstDate: new DateTime(2018),
       lastDate: new DateTime(2020),
     );
-    if(picked != null){
-      setState(() 
-        => _date  = picked
-      );
+    if (picked != null) {
+      //DateTime _date;
+  //String _dateFormated = DateFormat('dd/MM').format(_date);
+      String _dateFormated =  DateFormat('dd/MM/yyyy').format(picked);
+      _bloc.changeDate(_dateFormated.toString());
     }
   }
 }
